ea3ffd8810b3c14715405136b1afed18bbab61e8
El-gibbor <chiagoziem11@yahoo.com>
2023-05-09 10:24:35 +0400

    automated SQL login in py and sh

---
+++ automated_tasks/sqlLogin.sh
@@ -0,0 +1,41 @@
+#!/bin/env bash
+
+# Check if MySQL is already running
+if mysqladmin ping &>/dev/null; then
+    echo "MySQL is already running"
+else
+    echo "MySQL is NOT running"
+    if sudo service mysql start; then
+        echo "MySQL started successfully"
+    else
+        echo "Failed to start MySQL"
+        exit 1
+    fi
+fi
+
+# Prompt the user for MySQL credentials
+read -p "Enter MySQL username: " mysql_user
+read -s -p "Enter MySQL password: " mysql_password
+echo ""
+
+# Validate the credentials
+if ! mysql -u "$mysql_user" -p"$mysql_password" -e "SELECT 1" &>/dev/null; then
+    echo "Invalid MySQL credentials"
+    exit 1
+fi
+
+# Connect to MySQL
+mysql -u "$mysql_user" -p"$mysql_password"
+
+
+# Check if MySQL is already running
+# if mysqladmin ping &>/dev/null;
+# then
+#     echo "MySQL is already running"
+# else
+#     echo "MySQL is NOT running"
+#     sudo service mysql start
+# fi
+
+# # Connect to MySQL
+# mysql -u elgibbor -p
